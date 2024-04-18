55928d9790d0469ff8754ff083f2e98850580e38
Divyanshu Pandey <divyanshu1106@gmail.com>
2024-04-18 23:19:35 +0530

    Unit Testing

---
+++ src/main/java/ProductServiceApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ProductServiceApplication.class, args);
+    }
+}
+
+
+
---
+++ src/main/java/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
+
+@SpringBootApplication
